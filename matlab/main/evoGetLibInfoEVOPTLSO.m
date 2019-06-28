a2ffc2aca263d5979392f2c7963eb539c83107ac
DQQ-077-SZ <duanqq257@qq.com>
2019-06-28 18:46:26 +0800

    add main: evoGetLibInfoEVOPTLSO.m

---
+++ matlab/main/evoGetLibInfoEVOPTLSO.m
@@ -0,0 +1,8 @@
+function libInfo = evoGetLibInfoEVOPTLSO()
+% Get the basic Library Information of $evopt-lso$.
+libInfo.author = 'Qiqi Duan (DQQ / Q.Q. Duan)';
+libInfo.copyright = 'QiqiDuan@CSE.SUSTech.ShenZhen.China';
+libInfo.homepage = 'https://github.com/QiqiDuan257/evopt-lso';
+libInfo.version = '0.00.000.1';
+libInfo.createdate = 'June 28, 2019 (6-28-2019)';
+end
