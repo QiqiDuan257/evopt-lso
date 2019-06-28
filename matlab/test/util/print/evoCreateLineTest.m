846533c6360a55437dad92d5cb9d8b2794e00e29
DQQ-077-SZ <duanqq257@qq.com>
2019-06-28 19:19:22 +0800

    test print: evoCreateLine.m

---
+++ matlab/test/util/print/evoCreateLineTest.m
@@ -0,0 +1,13 @@
+clear; clc;
+
+%% Run:
+fprintf([evoCreateLine() '\n']);
+fprintf([evoCreateLine(7) '\n']);
+fprintf([evoCreateLine(5, '*') '\n']);
+fprintf([evoCreateLine(5, '*', '') '\n']);
+
+%% Outputs:
+% #############################################################################
+% #######
+% *****
+% accept at most two inputs.
