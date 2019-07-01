d83d8335f9cd4585603082d26f5b9ac3677f9c11
DQQ-077-SZ <duanqq257@qq.com>
2019-07-01 11:08:49 +0800

    add algo-core: evoCheckAlgoParam.m

---
+++ matlab/main/algo/core/evoCheckAlgoParam.m
@@ -0,0 +1,55 @@
+function algoParam = evoCheckAlgoParam(algoParam)
+% Check whether some basic settings of `algoParam` are right.
+
+% algoMaxFEval
+if ~isfield(algoParam, 'algoMaxFEval')
+    error('`algoMaxFEval` should be a field of `algoParam`.');
+end
+if ~evoCheckIsPosInt(algoParam.algoMaxFEval)
+    error('`algoMaxFEval` should be a positive integer.');
+end
+
+% algoIniSeed
+if ~isfield(algoParam, 'algoIniSeed')
+    seed = int64(str2double(datestr(now, 'yyyymmdd')));
+    rstream = RandStream('mt19937ar', 'Seed', seed);
+    algoParam.algoIniSeed = randi(rstream, intmax, 1, 1);
+end
+if ~evoCheckIsNonNegInt(algoParam.algoIniSeed)
+    error('`algoIniSeed` should be a nonnegative integer.');
+end
+
+% algoStopy
+if ~isfield(algoParam, 'algoStopy')
+    algoParam.algoStopy = -Inf;
+end
+stopy = algoParam.algoStopy;
+if ~isscalar(stopy) || ~isnumeric(stopy) || isnan(stopy)
+    error('`algoStopy` should be a non-nan numeric scalar.');
+end
+
+% algoStopt
+if ~isfield(algoParam, 'algoStopt')
+    algoParam.algoStopt = 7200; % seconds
+end
+algoStopt = algoParam.algoStopt;
+if ~isscalar(algoStopt) || ~isnumeric(algoStopt) || isnan(algoStopt)
+    error('`algoStopt` should be a non-nan numeric scalar.');
+end
+
+% algoOutFEval
+if ~isfield(algoParam, 'algoOutFEval')
+    algoParam.algoOutFEval = true; % output FEval curve
+end
+if ~isscalar(algoParam.algoOutFEval) || ~islogical(algoParam.algoOutFEval)
+    error('`algoOutFEval` should be a logical scalar.');
+end
+
+% algoOutFreq
+if ~isfield(algoParam, 'algoOutFreq')
+    algoParam.algoOutFreq = 1000; % output frequency of FEval curve
+end
+if ~evoCheckIsPosInt(algoParam.algoOutFreq)
+    error('`algoOutFreq` should be a positive integer.');
+end
+end
