e0d04953499982c180fb088835b0a7f3a2939d0c
DQQ-077-SZ <duanqq257@qq.com>
2019-07-08 16:11:30 +0800

    test print: evoCreateFEvalFile.m

---
+++ matlab/test/util/print/evoCreateFEvalFileTest.m
@@ -0,0 +1,9 @@
+clear; clc;
+
+%%
+disp(evoCreateFEvalFile('evoRS')); % ./.evoFEvalFolder/evoRS.mat
+% after running, a new directory called '.evoFEvalFolder' will be added in
+% the current working directory
+
+%%
+disp(evoCreateFEvalFile('evoRS')); % ./.evoFEvalFolder/evoRS.mat
