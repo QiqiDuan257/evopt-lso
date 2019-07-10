249395db12e61000d90360655f2ea46e9c287fe6
DQQ-077-SZ <duanqq257@qq.com>
2019-07-10 19:51:31 +0800

    add evoCreateEnvBaseFunc.m

---
+++ matlab/main/test-env/func/base/evoCreateEnvBaseFunc.m
@@ -0,0 +1,27 @@
+function optRes = evoCreateEnvBaseFunc(funcParam, algoParam)
+% Create an Environment for the Base Function with running the algorithm once.
+%
+% Inputs ->
+%   funcParam : function parameters (struct)
+%   algoParam : algorithm parameters (struct)
+% Outputs <-
+%   optRes    : optimization results (struct)
+
+% load `funcParam`
+funcParam = evoCheckFuncParam(funcParam);
+
+% load `algoParam`
+algoName = algoParam.algoName;
+maxFEval = algoParam.algoMaxFEval;
+
+% optimize
+fprintf('*** funcDim: %d + algoName: %s + maxFEval: %d ***\n', ...
+    funcParam.funcDim, algoName, maxFEval);
+optRes = feval(algoName, funcParam, algoParam);
+logInfo = ['    opty = %+7.4e | runtime = %7.2e ' ...
+    '| timeFEval = %7.2e | numFEval = %d '...
+    '| timeOutFEval = %7.2e | stopFlag = %s\n'];
+fprintf(logInfo, optRes.opty, optRes.runtime, ...
+    optRes.timeFEval, optRes.numFEval, ...
+    optRes.timeOutFEval, optRes.stopFlag);
+end
