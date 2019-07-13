5007ded22048896c25c1e1fc340438ae46434e61
DQQ-077-SZ <duanqq257@qq.com>
2019-07-13 20:32:43 +0800

    add more comments

--- matlab/main/util/print/evoCreateLine.m
+++ matlab/main/util/print/evoCreateLine.m
@@ -1,5 +1,13 @@
 function line = evoCreateLine(varargin)
 % Create a Line with the symbol repeatedly printed.
+%
+% Input ->
+%   symbol  : symbol needed to repeatedly print (char scalar)
+%             optional, default: '#'
+%   lenLine : times of repeating the symbol (integer scalar)
+%             optional, default: 77
+% Output <-
+%   line    : a set of the same symbol (row char vector)
 if nargin == 0
     lenLine = 77; % just a number preferred by the designer of this library
     symbol = '#'; % just a symbol preferred by the designer of this library
@@ -12,5 +20,13 @@ elseif nargin == 2
 else
     error('accept at most two inputs.');
 end
+
+if ~isscalar(symbol) || ~ischar(symbol)
+    error('`symbol` should be a char scalar.');
+end
+if ~evoCheckIsPosInt(lenLine)
+    error('`lenLine` should be a positive integer.');
+end
+
 line = repmat(symbol, 1, lenLine);
 end
