a0a1ffd8e1a63a4f25cabface24d32d002b60754
DQQ-077-SZ <duanqq257@qq.com>
2019-07-16 01:26:18 +0800

    add algo-base: evoCheckR1A1ES.m

---
+++ matlab/main/algo/base/evoCheckR1A1ES.m
@@ -0,0 +1,45 @@
+function [algoParam, maxFEval, stopy, stopt, iniX, stepSize, corXO] = ...
+    evoCheckR1A1ES(algoParam, funcParam)
+% Check whether all the settings of `algoParam` are right for R1A1ES.
+[algoParam, maxFEval, iniSeed, stopy, stopt] = ...
+    evoCheckGetAlgoParam(algoParam);
+
+funcDim = funcParam.funcDim;
+funcIniUB = funcParam.funcIniUB;
+funcIniLB = funcParam.funcIniLB;
+
+% algoIniX
+if ~isfield(algoParam, 'algoIniX')
+    rstream = RandStream('mt19937ar', 'Seed', iniSeed);
+    algoParam.algoIniX = funcIniLB + (funcIniUB - funcIniLB) .* ...
+        rand(rstream, funcDim, 1);
+end
+iniX = algoParam.algoIniX;
+if ~isnumeric(iniX) || any(size(iniX) ~= [funcDim 1])
+    error('`algoIniX` should be a %d * 1 numeric vector.', funcDim);
+end
+if any(iniX > funcIniUB)
+    error('`algoIniX` should not be out of upper search bound.');
+end
+if any(iniX < funcIniLB)
+    error('`algoIniX` should not be out of lower search bound.');
+end
+
+% algoStepSize
+if ~isfield(algoParam, 'algoStepSize')
+    algoParam.algoStepSize = 1e-1;
+end
+stepSize = algoParam.algoStepSize;
+if ~isscalar(stepSize) || ~isnumeric(stepSize)
+    error('`algoStepSize` should be a numeric scalar.');
+end
+
+% algoCorXO
+if ~isfield(algoParam, 'algoCorXO')
+    algoParam.algoCorXO = false;
+end
+corXO = algoParam.algoCorXO;
+if ~isscalar(corXO) || ~islogical(corXO)
+    error('`algoCorXO` should be a logical scalar.');
+end
+end
