1fc13a72ae1cc6aad4a26f21787c6155c81a234a
DQQ-077-SZ <duanqq257@qq.com>
2019-07-15 09:48:05 +0800

    update algo-base: Random Search (RS)

--- matlab/main/algo/base/evoRS.m
+++ matlab/main/algo/base/evoRS.m
@@ -6,31 +6,28 @@ function optRes = evoRS(funcParam, algoParam)
 %   algoParam : algorithm parameters (struct)
 % Output <-
 %   optRes    : optimization results (struct)
-startRun = tic;
-timeOutFEval = NaN; % runtime of outputting FEval curve
-curveFEval = [];
+startRun = tic; % start timing
+ccFEval = []; % initialize convergence curve of function evaluations
 
-% load `funcParam`
+% load `funcParam` + `algoParam`
 [funcParam, funcName, ~, funcUB, funcLB] = evoCheckGetFuncParam(funcParam);
-
-% load `algoParam`
-[maxFEval, ~, stopy, stopt, outFEval, outFreq, popSize, X, outFile] = ...
-    evoCheckRS(algoParam, funcParam);
+[algoParam, maxFEval, stopy, stopt, popSize, X] = evoCheckRS(...
+    algoParam, funcParam);
 
 % set UB and LB for all individuals during search
-funcUB = repmat(funcUB, 1, popSize);
-funcLB = repmat(funcLB, 1, popSize);
+funcUB = repmat(funcUB, 1, popSize); % upper search bound
+funcLB = repmat(funcLB, 1, popSize); % lower search bound
 
 % initialize fitness (i.e., y)
 startFEval = tic;
 y = feval(funcName, X);
 timeFEval = toc(startFEval);
 numFEval = size(X, 2);
-if outFEval
-    startOutFEval = tic;
-    curveFEval = cat(1, curveFEval, y');
-    timeOutFEval = toc(startOutFEval);
-end
+
+% update convergence curve of function evaluations
+startCcFEval = tic;
+ccFEval = cat(1, ccFEval, y');
+timeCcFEval = toc(startCcFEval);
 
 % initialize optx + opty
 [opty, yMinInd] = min(y);
@@ -39,26 +36,31 @@ optx = X(:, yMinInd);
 % iteratively optimize
 stopFlag = 'maxFEval';
 while numFEval < maxFEval
+    % stop after reaching the threshold for the best-fo-far function value
     if opty <= stopy
         stopFlag = 'stopy';
         break;
     end
     
+    % stop after reaching the threshold for the maximal allowable runtime
     if toc(startRun) >= stopt
         stopFlag = 'stopt';
         break;
     end
     
+    % generate uniformly distributed random samples (i.e., X)
     X = unifrnd(funcLB, funcUB);
+    
+    % evaluate these new samples (i.e., X) and get their fitness
     startFEval = tic;
     y = feval(funcName, X);
     timeFEval = timeFEval + toc(startFEval);
     numFEval = numFEval + size(X, 2);
-    if outFEval
-        startOutFEval = tic;
-        curveFEval = cat(1, curveFEval, y');
-        timeOutFEval = timeOutFEval + toc(startOutFEval);
-    end
+    
+    % update convergence curve of function evaluations
+    startCcFEval = tic;
+    ccFEval = cat(1, ccFEval, y');
+    timeCcFEval = timeCcFEval + toc(startCcFEval);
     
     % update optx + opty
     [yMin, yMinInd] = min(y);
@@ -68,30 +70,15 @@ while numFEval < maxFEval
     end
 end
 
-% output FEval curve
-if outFEval
-    if isfield(algoParam, 'algoQuietOutFEval') && ...
-            algoParam.algoQuietOutFEval
-        % do not save `curveFEval` in the external file system
-    else
-        startOutFEval = tic;
-        save(evoCreateFEvalFile(outFile), 'curveFEval');
-        curveFEval = evoDecNumVect(curveFEval);
-        outFreqVect = evoGetOutFreqVect(outFreq, numFEval);
-        curveFEval = cat(2, outFreqVect, curveFEval(outFreqVect));
-        timeOutFEval = timeOutFEval + toc(startOutFEval);
-    end
-end
-
-% return optimization results
+% return optimization results (optRes)
 optRes.opty = opty;
 optRes.optx = optx;
 optRes.timeFEval = timeFEval;
 optRes.numFEval = numFEval;
-optRes.curveFEval = curveFEval;
-optRes.timeOutFEval = timeOutFEval;
-optRes.stopFlag = stopFlag;
+optRes.ccFEval = ccFEval;
+optRes.timeCcFEval = timeCcFEval;
 optRes.funcParam = funcParam;
 optRes.algoParam = algoParam;
-optRes.runtime = toc(startRun);
+optRes.stopFlag = stopFlag;
+optRes.runtime = toc(startRun); % end timing
 end
