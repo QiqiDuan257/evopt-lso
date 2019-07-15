1758933be101e01ed18fec470dfb35308265cb91
DQQ-077-SZ <duanqq257@qq.com>
2019-07-16 01:38:58 +0800

    add algo-base: Rechenberg's (1+1)-Evolution Strategy (R1A1ES)

---
+++ matlab/main/algo/base/evoR1A1ES.m
@@ -0,0 +1,95 @@
+function optRes = evoR1A1ES(funcParam, algoParam)
+% Rechenberg's (1+1)-Evolution Strategy (R1A1ES).
+%
+% Input ->
+%   funcParam : function parameters (struct)
+%   algoParam : algorithm parameters (struct)
+% Output <-
+%   optRes    : optimization results (struct)
+startRun = tic; % start timing
+ccFEval = []; % initialize convergence curve of function evaluations
+
+numXO = 0; % number of out-of-bound solutions
+numCorXO = 0; % number of correcting out-of-bound solutions
+timeCorXO = 0; % time of correcting out-of-bound solutions
+
+% load `funcParam` + `algoParam`
+[funcParam, funcName, funcDim, funcUB, funcLB] = ...
+    evoCheckGetFuncParam(funcParam);
+[algoParam, maxFEval, stopy, stopt, optx, stepSize, corXO] = ...
+    evoCheckR1A1ES(algoParam, funcParam);
+
+% initialize opty
+startFEval = tic;
+opty = feval(funcName, optx);
+timeFEval = toc(startFEval);
+numFEval = size(optx, 2);
+
+% update convergence curve of function evaluations
+startCcFEval = tic;
+ccFEval = cat(1, ccFEval, opty);
+timeCcFEval = toc(startCcFEval);
+
+% iteratively optimize
+stopFlag = 'maxFEval';
+while numFEval < maxFEval
+    % stop after reaching the threshold for the best-fo-far function value
+    if opty <= stopy
+        stopFlag = 'stopy';
+        break;
+    end
+    
+    % stop after reaching the threshold for the maximal allowable runtime
+    if toc(startRun) >= stopt
+        stopFlag = 'stopt';
+        break;
+    end
+    
+    % mutate the best-so-far solution (i.e., optx)
+    x = optx + normrnd(zeros(funcDim, 1), stepSize);
+    
+    % check out-of-bound solutions
+    startCorXO = tic;
+    indXOB = (x > funcUB) | (x < funcLB);
+    if any(indXOB)
+        numXO = numXO + 1;
+        if corXO % correct out-of-bound solutions
+            x(indXOB) = unifrnd(funcLB(indXOB), funcUB(indXOB));
+            numCorXO = numCorXO + 1;
+        end
+    end
+    timeCorXO = timeCorXO + toc(startCorXO);
+    
+    % evaluate the new sample (i.e., x) and get its fitness
+    startFEval = tic;
+    y = feval(funcName, x);
+    timeFEval = timeFEval + toc(startFEval);
+    numFEval = numFEval + size(x, 2);
+    
+    % update convergence curve of function evaluations
+    startCcFEval = tic;
+    ccFEval = cat(1, ccFEval, y);
+    timeCcFEval = timeCcFEval + toc(startCcFEval);
+    
+    % update optx + opty
+    if opty > y
+        opty = y;
+        optx = x;
+    end
+end
+
+% return optimization results (optRes)
+optRes.opty = opty;
+optRes.optx = optx;
+optRes.timeFEval = timeFEval;
+optRes.numFEval = numFEval;
+optRes.ccFEval = ccFEval;
+optRes.timeCcFEval = timeCcFEval;
+optRes.funcParam = funcParam;
+optRes.algoParam = algoParam;
+optRes.stopFlag = stopFlag;
+optRes.numXO = numXO;
+optRes.numCorXO = numCorXO;
+optRes.timeCorXO = timeCorXO;
+optRes.runtime = toc(startRun); % end timing
+end
