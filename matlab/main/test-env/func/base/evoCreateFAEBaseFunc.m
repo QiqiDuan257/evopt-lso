5e7b659b9df336ec22a5aec24f1c5b5ac6f365b4
DQQ-077-SZ <duanqq257@qq.com>
2019-07-15 10:34:42 +0800

    update evoCreateFAEBaseFunc.m

--- matlab/main/test-env/func/base/evoCreateFAEBaseFunc.m
+++ matlab/main/test-env/func/base/evoCreateFAEBaseFunc.m
@@ -1,12 +1,26 @@
 function [statOptRes, funcParams, algoParams] = ...
     evoCreateFAEBaseFunc(funcParam, algoParam)
 % Create a set of `FuncParam`, a set of `AlgoParam`, and
-% an Environment for the Base Function
+% an Environment for the Base Function.
+timeFEvalFile = 0;
+
 funcParams = evoCreateFuncParamsBaseFunc(funcParam);
 algoParams = evoCreateAlgoParamsBaseFunc(funcParams, algoParam);
 statOptRes = cell(length(funcParams), 1);
+
 for f = 1 : length(funcParams)
     statOptRes{f} = evoCreateEnvBaseFunc(funcParams{f}, algoParams{f});
+    
+    startFEvalFile = tic;
+    ccFEvalFile = sprintf('%s_%s_D%d', algoParam.algoName, ...
+        funcParam.funcName, funcParams{f}.funcDim);
+    ccFEval = statOptRes{f}.ccFEval;
+    save(evoCreateFEvalFile(ccFEvalFile), 'ccFEval');
+    statOptRes{f}.ccFEval = evoZipFEvalFile(ccFEval, statOptRes{f}.numFEval);
+    statOptRes{f}.algoParam = rmfield(statOptRes{f}.algoParam, 'algoIniX');
+    timeFEvalFile = timeFEvalFile + toc(startFEvalFile);
 end
-save(algoParam.algoName, 'statOptRes', 'funcParams', 'algoParams', '-v7.3');
+
+save(algoParam.algoName, 'statOptRes', ...
+    'funcParams', 'algoParams', 'timeFEvalFile', '-v7.3');
 end
