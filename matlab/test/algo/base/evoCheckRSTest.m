fad6062abfd11c1d7d08f9dd85f8b814d0b92942
DQQ-077-SZ <duanqq257@qq.com>
2019-07-15 10:56:01 +0800

    update algo-base: evoCheckRS.m

--- matlab/test/algo/base/evoCheckRSTest.m
+++ matlab/test/algo/base/evoCheckRSTest.m
@@ -6,40 +6,26 @@ algoParam.algoMaxFEval = 2000;
 algoParam.algoIniSeed = 7;
 algoParam.algoStopy = 20;
 algoParam.algoStopt = 120;
-algoParam.algoOutFEval = false;
-algoParam.algoOutFreq = 100;
-funcParam.funcName = 'evoFuncSphere';
-funcParam.funcDim = 2;
-funcParam.funcIniUB = [5 5]';
-funcParam.funcIniLB = [-2 -2]';
-[maxFEval, iniSeed, stopy, stopt, outFEval, outFreq, ...
-    popSize, iniX, outFile] = evoCheckRS(algoParam, funcParam);
+funcParam = evoFuncParams('', 2, 5, -2);
+[algoParam, maxFEval, stopy, stopt, popSize, iniX] = ...
+    evoCheckRS(algoParam, funcParam);
 disp(maxFEval); % 2000
-disp(iniSeed); % 7
 disp(stopy); % 20
 disp(stopt); % 120
-disp(outFEval); % 0
-disp(outFreq); % 100
 disp(popSize); % 1000
 disp(size(iniX)); % 2 1000
-disp(outFile); % evoRS_evoFuncSphere_D2
 
 %%
 clear; clc;
 algoParam.algoMaxFEval = 100;
 algoParam.algoIniSeed = 7;
 algoParam.algoPopSize = 3;
-funcParam.funcDim = 2;
-funcParam.funcIniUB = [5 5]';
-funcParam.funcIniLB = [-2 -2]';
-[maxFEval, iniSeed, stopy, stopt, outFEval, outFreq, ...
-    popSize, iniX] = evoCheckRS(algoParam, funcParam);
+funcParam = evoFuncParams('', 2, 5, -2);
+[algoParam, maxFEval, stopy, stopt, popSize, iniX] = ...
+    evoCheckRS(algoParam, funcParam);
 disp(maxFEval); % 100
-disp(iniSeed); % 7
 disp(stopy); % -Inf
 disp(stopt); % 7200
-disp(outFEval); % 1
-disp(outFreq); % 1000
 disp(popSize); % 3
 disp(iniX);
 %    -1.4658    1.0689    4.8459
@@ -49,9 +35,7 @@ disp(iniX);
 clear; clc;
 algoParam.algoMaxFEval = 100;
 algoParam.algoPopSize = 0;
-funcParam.funcDim = 2;
-funcParam.funcIniUB = [5 5]';
-funcParam.funcIniLB = [-2 -2]';
+funcParam = evoFuncParams('', 2, 5, -2);
 evoCheckRS(algoParam, funcParam);
 
 %%
@@ -60,17 +44,12 @@ algoParam.algoMaxFEval = 100;
 algoParam.algoIniSeed = 7;
 algoParam.algoPopSize = 3;
 algoParam.algoIniX = [1 2 3; 1 2 3];
-funcParam.funcDim = 2;
-funcParam.funcIniUB = [5 5]';
-funcParam.funcIniLB = [-2 -2]';
-[maxFEval, iniSeed, stopy, stopt, outFEval, outFreq, ...
-    popSize, iniX] = evoCheckRS(algoParam, funcParam);
+funcParam = evoFuncParams('', 2, 5, -2);
+[algoParam, maxFEval, stopy, stopt, popSize, iniX] = ...
+    evoCheckRS(algoParam, funcParam);
 disp(maxFEval); % 100
-disp(iniSeed); % 7
 disp(stopy); % -Inf
 disp(stopt); % 7200
-disp(outFEval); % 1
-disp(outFreq); % 1000
 disp(popSize); % 3
 disp(iniX);
 %      1     2     3
@@ -82,9 +61,7 @@ algoParam.algoMaxFEval = 100;
 algoParam.algoIniSeed = 7;
 algoParam.algoPopSize = 3;
 algoParam.algoIniX = [1 2 3];
-funcParam.funcDim = 2;
-funcParam.funcIniUB = [5 5]';
-funcParam.funcIniLB = [-2 -2]';
+funcParam = evoFuncParams('', 2, 5, -2);
 evoCheckRS(algoParam, funcParam);
 
 %% `algoIniX` should not be out of upper search bound.
@@ -93,9 +70,7 @@ algoParam.algoMaxFEval = 100;
 algoParam.algoIniSeed = 7;
 algoParam.algoPopSize = 3;
 algoParam.algoIniX = [1 2 30; 1 2 3];
-funcParam.funcDim = 2;
-funcParam.funcIniUB = [5 5]';
-funcParam.funcIniLB = [-2 -2]';
+funcParam = evoFuncParams('', 2, 5, -2);
 evoCheckRS(algoParam, funcParam);
 
 %% `algoIniX` should not be out of lower search bound.
@@ -104,30 +79,5 @@ algoParam.algoMaxFEval = 100;
 algoParam.algoIniSeed = 7;
 algoParam.algoPopSize = 3;
 algoParam.algoIniX = [1 2 3; 1 2 -3];
-funcParam.funcDim = 2;
-funcParam.funcIniUB = [5 5]';
-funcParam.funcIniLB = [-2 -2]';
-evoCheckRS(algoParam, funcParam);
-
-%%
-clear; clc;
-algoParam.algoMaxFEval = 2000;
-algoParam.algoOutFile = 'FEval';
-funcParam = evoFuncParams('evoFuncSphere', 2, 5, 2);
-[maxFEval, iniSeed, stopy, stopt, outFEval, outFreq, ...
-    popSize, iniX, outFile] = evoCheckRS(algoParam, funcParam);
-disp(maxFEval); % 2000
-disp(stopy); % -Inf
-disp(stopt); % 7200
-disp(outFEval); % 1
-disp(outFreq); % 1000
-disp(popSize); % 1000
-disp(size(iniX)); % 2 1000
-disp(outFile); % FEval
-
-%% `algoOutFile` should be of char or string type.
-clear; clc;
-algoParam.algoMaxFEval = 2000;
-algoParam.algoOutFile = true;
-funcParam = evoFuncParams('evoFuncSphere', 2, 5, 2);
+funcParam = evoFuncParams('', 2, 5, -2);
 evoCheckRS(algoParam, funcParam);
