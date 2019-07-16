7555763085778d1222773b9944e342efeabd0db5
DQQ-077-SZ <duanqq257@qq.com>
2019-07-16 15:52:32 +0800

    update evoCompRuntimeBaseFunc.m

--- matlab/main/test-env/func/base/evoCompRuntimeBaseFunc.m
+++ matlab/main/test-env/func/base/evoCompRuntimeBaseFunc.m
@@ -1,7 +1,7 @@
 function evoCompRuntimeBaseFunc(varargin)
-% Compare (plot) different Runtime on the Base Function.
+% Compare (plot) Runtime on the Base Function.
 startRun = tic;
-fprintf([evoCreateLine(14, '#') ' * Runtime * ' evoCreateLine(14, '#') '\n']);
+fprintf([evoCreateLine(14) ' * runtime * ' evoCreateLine(14) '\n']);
 figure(20190702);
 DATA = load(varargin{1});
 funcParams = DATA.funcParams;
@@ -22,6 +22,8 @@ for a = 1 : (nargin - 1)
     ax.ColorOrderIndex = a;
     axs(a) = plot(funcDims, runtimes(:, a), '*', 'LineWidth', 7); hold on;
     plot(funcDims, runtimes(:, a), 'k:'); hold on;
+    fprintf('   %12s: %7.5e\n', ...
+        statOptRes{1}.algoParam.algoName, sum(runtimes(:, a)));
 end
 legend(axs, labels); title('total runtime');
 xlabel('funcDim'); ylabel('seconds');
