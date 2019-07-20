2bf325cb79de0acb886fb1a7e3c86a1d3c92c14f
DQQ-077-SZ <duanqq257@qq.com>
2019-07-20 15:06:14 +0800

    test algo-base: *Pure* Covariance Matrix Adaptation Evolution Strategy (P-CMA-ES)

--- matlab/test/algo/base/evoPCMAESTest.m
+++ matlab/test/algo/base/evoPCMAESTest.m
@@ -7,8 +7,10 @@ rng(20190101);
 funcParam = evoFuncParams('evoFuncSphere', 2, 100);
 algoParam.algoMaxFEval = 1e5 * funcParam.funcDim;
 algoParam.algoIniSeed = 20190701;
+algoParam.algoRecStepSize = true;
+algoParam.algoRecCov = true;
 optRes = evoPCMAES(funcParam, algoParam);
 fprintf('Total runtime: %5.2e.\n', toc(statRun));
 disp(evoCheckIsClose(evoFuncSphere(optRes.optx), optRes.opty)); % 1
 disp(optRes.opty); % 0
-disp(optRes.curveFEval(1, 2)); % 2.1154e+03
+disp(optRes.ccFEval(1)); % 2.1154e+03
