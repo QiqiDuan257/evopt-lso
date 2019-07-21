74057a52927385d6f1abcbe86f6e4892da5909ef
DQQ-077-SZ <duanqq257@qq.com>
2019-07-21 18:35:54 +0800

    run PCMAES on rosenbrock

---
+++ matlab/test/func/base/rosenbrock/demo/evoPCMAESRun.m
@@ -0,0 +1,12 @@
+clear; clc;
+
+% nohup matlab -nodesktop -nodisplay -nosplash <evoPCMAESRun.m >evoPCMAESRun.out 2>&1 &
+
+startRun = tic;
+rng(20190101); % for reproducibility
+funcParam.funcName = 'evoFuncRosenbrock';
+% funcParam.funcDims = [2 10];
+algoParam.algoName = 'evoPCMAES';
+[statOptRes, funcParams, algoParams] = ...
+    evoCreateFAEBaseFunc(funcParam, algoParam);
+fprintf(sprintf('$-runtime-$: %7.5e.\n', toc(startRun)));
