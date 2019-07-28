3b5c1b276f2698ebbd798fd87695bb5bccde7043
DQQ-077-SZ <duanqq257@qq.com>
2019-07-28 20:22:40 +0800

    test divide-conquer: evoDiCoH2CyclicRandTest.m

---
+++ matlab/test/algo/divide-conquer/evoDiCoH2CyclicRandTest.m
@@ -0,0 +1,41 @@
+clear; clc;
+
+%%
+clear; clc;
+funcParam.funcDim = 11;
+funcParam.funcNumPart = 2;
+funcParam.funcPartSet = {[11 10 1 2]'; 8; [3 7 5]'; [4 6]'; 9};
+funcParam.funcPartType = {'nonSep'; 'sep'; 'sep'; 'nonSep'; 'sep'};
+funcParam = evoDiCoH2CyclicRand(funcParam);
+
+%%
+clear; clc;
+funcParam.funcDim = 10;
+funcParam.funcNumPart = 5;
+funcParam.funcPartSet = {(10 : -1 : 1)'};
+funcParam.funcPartType = {'sep'};
+funcParam = evoDiCoH2CyclicRand(funcParam);
+
+%%
+clear; clc;
+funcParam.funcDim = 10;
+funcParam.funcNumPart = 5;
+funcParam.funcPartSet = {(10 : -1 : 1)'};
+funcParam.funcPartType = {'nonSep'};
+funcParam = evoDiCoH2CyclicRand(funcParam);
+
+%%
+clear; clc;
+funcParam.funcDim = 10;
+funcParam.funcNumPart = 3;
+funcParam.funcPartSet = {(10 : -1 : 1)'};
+funcParam.funcPartType = {'sep'};
+funcParam = evoDiCoH2CyclicRand(funcParam);
+
+%%
+clear; clc;
+funcParam.funcDim = 10;
+funcParam.funcNumPart = 3;
+funcParam.funcPartSet = {(10 : -1 : 1)'};
+funcParam.funcPartType = {'nonSep'};
+funcParam = evoDiCoH2CyclicRand(funcParam);
