3dde9218b3cd6cb065c5fbfe3a812ad03f086860
DQQ-077-SZ <duanqq257@qq.com>
2019-07-29 19:54:55 +0800

    test divide-conquer: evoDiCoH2CyclicRandTest.m

--- matlab/test/algo/divide-conquer/evoDiCoH2CyclicRandTest.m
+++ matlab/test/algo/divide-conquer/evoDiCoH2CyclicRandTest.m
@@ -39,3 +39,11 @@ funcParam.funcNumPart = 3;
 funcParam.funcPartSet = {(10 : -1 : 1)'};
 funcParam.funcPartType = {'nonSep'};
 funcParam = evoDiCoH2CyclicRand(funcParam);
+
+%%
+clear; clc;
+funcParam.funcDim = 112;
+funcParam.funcNumPart = 2;
+funcParam.funcPartSet = {1; 2; [3 4 5]'; (6 : 10)'; (11 : 112)'};
+funcParam.funcPartType = {'sep'; 'sep'; 'nonSep'; 'sep'; 'nonSep'};
+funcParam = evoDiCoH2CyclicRand(funcParam);
