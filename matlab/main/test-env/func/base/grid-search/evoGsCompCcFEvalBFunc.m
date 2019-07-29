e9155507df8d6faf0046ecb46faccb4f80945244
DQQ-077-SZ <duanqq257@qq.com>
2019-07-29 20:07:26 +0800

    add grid-search: evoGsCompCcFEvalBFunc.m

---
+++ matlab/main/test-env/func/base/grid-search/evoGsCompCcFEvalBFunc.m
@@ -0,0 +1,41 @@
+function evoGsCompCcFEvalBFunc(varargin)
+% Grid search: Compare Curve of Function Evaluations on the Base Function.
+startRun = tic;
+fprintf([evoCreateLine(14) ' * ccFEval * ' evoCreateLine(14) '\n']);
+figure(20190701);
+DATA = load(varargin{1});
+funcParams = DATA.funcParams;
+labels = varargin{nargin};
+for f = 1 : length(funcParams)
+    subplot(3, 3, f);
+    maxFEval = 0;
+    numLabel = 0;
+    for a = 1 : (nargin - 1)
+        DATA = load(varargin{a});
+        statOptRes = DATA.statOptRes;
+        for r = 1 : length(statOptRes)
+            if statOptRes{r}.funcParam.funcDim == funcParams{f}.funcDim
+                if statOptRes{r}.ccFEval(end, 1) > maxFEval
+                    maxFEval = statOptRes{r}.ccFEval(end, 1);
+                end
+                numLabel = numLabel + 1;
+            end
+        end
+    end
+    for a = 1 : (nargin - 1)
+        DATA = load(varargin{a});
+        statOptRes = DATA.statOptRes;
+        for r = 1 : length(statOptRes)
+            if statOptRes{r}.funcParam.funcDim == funcParams{f}.funcDim
+                semilogy(statOptRes{r}.ccFEval(:, 1), ...
+                    statOptRes{r}.ccFEval(:, 2), ...
+                    'LineWidth', 1.5);
+                xlim([0 maxFEval]); hold on;
+            end
+        end
+    end
+    legend(labels{1 : numLabel}); title(funcParams{f}.funcDim);
+    xlabel('No. of FEval'); ylabel('opty');
+end
+fprintf(sprintf('$-runtime-$: %7.5e.\n', toc(startRun)));
+end
