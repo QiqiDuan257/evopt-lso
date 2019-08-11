82879918043211935d912dab0ee94235c6f44d4c
DQQ-077-SZ <duanqq257@qq.com>
2019-08-11 15:57:59 +0800

    add func: l1-rotated_ellipsoid

---
+++ matlab/test/func/base/l1-rotated_ellipsoid/evoL1_RF_Ellipsoid.m
@@ -0,0 +1,19 @@
+function y = evoL1_RF_Ellipsoid(X)
+persistent RM sv;
+[funcDim, popSize] = size(X);
+if isempty(RM) || isempty(sv) || (size(RM, 1) ~= funcDim) || ...
+        (size(RM, 2) ~= funcDim)  || (size(sv, 1) ~= funcDim)
+    SRM = load(sprintf('./.evoRatationShift/evoRF_Ellipsoid_R%d.mat', funcDim));
+    RM = SRM.RM;
+    if any(size(RM) ~= [funcDim funcDim])
+        error('`RM` should be a %d * %d matrix.', funcDim, funcDim);
+    end
+    SSV = load(sprintf('./.evoRatationShift/evoRF_Ellipsoid_s%d.mat', funcDim));
+    sv = SSV.sv;
+    if any(size(sv) ~= [funcDim 1])
+        error('`sv` should be a %d * 1 vector.', funcDim);
+    end
+    sv = repmat(sv, 1, popSize);
+end
+y = evoFuncEllipsoid(RM * (X - sv)) + evoFuncReg1Norm(X);
+end
---
+++ matlab/test/func/base/l1-rotated_ellipsoid/evoL1_RF_Ellipsoid2.m
@@ -0,0 +1,20 @@
+function y = evoL1_RF_Ellipsoid2(X)
+persistent RM sv;
+[funcDim, popSize] = size(X);
+if isempty(RM)  || (size(RM, 1) ~= funcDim) || (size(RM, 2) ~= funcDim)
+    SRM = load(sprintf('./.evoRatationShift/evoRF_Ellipsoid_R%d.mat', funcDim));
+    RM = SRM.RM;
+    if any(size(RM) ~= [funcDim funcDim])
+        error('`RM` should be a %d * %d matrix.', funcDim, funcDim);
+    end
+end
+if isempty(sv) || (size(sv, 1) ~= funcDim) || (size(sv, 2) ~= popSize)
+    SSV = load(sprintf('./.evoRatationShift/evoRF_Ellipsoid_s%d.mat', funcDim));
+    sv = SSV.sv;
+    if any(size(sv) ~= [funcDim 1])
+        error('`sv` should be a %d * 1 vector.', funcDim);
+    end
+    sv = repmat(sv, 1, popSize);
+end
+y = evoFuncEllipsoid(RM * (X - sv)) + evoFuncReg1Norm(X);
+end
